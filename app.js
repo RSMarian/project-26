bc9e9d194de131ca9d8e307138770ad341a79496
RSMarian <marianstefan1499@gmail.com>
2022-07-07 13:39:03 +0300

    fixed positioning

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const slideLeft = document.querySelector(".left-slide");
 const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
 const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
